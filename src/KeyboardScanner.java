01fc422c7d20c06f6054594ae3b8ffa2956541cd
user <user@FC11>
2016-10-07 08:56:57 +0800

    Exercise 1

---
+++ src/KeyboardScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class KeyboardScanner {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int Integer;
+		float Floatpoint;
+		String Name;
+		System.out.printf("Enter a integer: ");
+		Integer = scanner.nextInt();
+		System.out.printf("Enter a float point number: ");
+		Floatpoint = scanner.nextFloat();
+		System.out.printf("Enter a you name: ");
+		Name = scanner.next();
+		System.out.printf("Hi %s, the multiplication of %d and %f is %e",Name,Integer,Floatpoint,Integer*Floatpoint);
+	}
+}
