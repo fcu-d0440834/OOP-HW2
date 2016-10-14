69739baceeb34c7a350d4acfad932066d8eee47c
user <user@FC11>
2016-10-14 10:01:17 +0800

    10/14 homework \n teacher is handsome

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -6,16 +6,24 @@ public class StringComparator {
 
 		Scanner scanner = new Scanner(System.in);
 		String str1,str2;
+		int flag=0;
+		while(flag==0)
+		{
+			
+		
 		System.out.printf("Enter a string 1:");
 		str1=scanner.nextLine();
 		System.out.printf("Enter a string 2:");
 		str2=scanner.nextLine();
 		
 		if((str1.compareToIgnoreCase(str2))==0)
+		{
 			System.out.printf("The two strings are the same.");	
+			flag=1;
+		}			
 		else
-			System.out.printf("The two strings are not the same.");
-		
+			System.out.printf("The two strings are not the same.\n\n");
+		}
 			
 	}
 
